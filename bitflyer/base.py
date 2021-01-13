14eecf9f09f208ce97af69e4db979c55391ebd14
ishtos <ginkgonut.9960@gmail.com>
2021-01-14 06:45:54 +0900

    intergrate public and private

--- bitflyer/base.py
+++
@@ -1,9 +0,0 @@
-class Base:
-    url = "https://api.bitflyer.com"
-     
-    @staticmethod
-    def parse_response(r):
-        if r.text:
-            return {"status_code": r.status_code, "response": r.json()}
-        else:
-            return {"status_code": r.status_code}
