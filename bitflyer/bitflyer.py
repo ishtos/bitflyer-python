192c7bc8d0a037e9442d3882ae0521affa0575e1
ishtos <ginkgonut.9960@gmail.com>
2021-01-24 22:50:51 +0900

    add comment

--- bitflyer/bitflyer.py
+++ bitflyer/bitflyer.py
@@ -7,15 +7,14 @@ import requests
 
 
 class API:
-    url = "https://api.bitflyer.com"
- 
+
     def __init__(self, api_key=None, api_secret=None):
+        self.url = "https://api.bitflyer.com"
         self.api_key = api_key
         self.api_secret = api_secret
 
     def markets(self):
-        """Market List - マーケットの一覧
-        """
+        """Market List - マーケットの一覧"""
         return self.public_get(path="/v1/getmarkets")
 
     def board(self, params={"product_code": "BTC_JPY"}):
@@ -55,7 +54,7 @@ class API:
 
     def health(self):
         """Exchange status - 取引所の状態
-        
+
         Parameters:
             product_code: Specifies a product_code or alias, as obtained from the Market List.
         """
@@ -70,130 +69,106 @@ class API:
         return self.public_get(path="/v1/getchats")
 
     def getpermissions(self):
-        """Get API Key Permissions - API キーの権限を取得
-        """
+        """Get API Key Permissions - API キーの権限を取得"""
         return self.private_get("/v1/me/getpermissions")
 
     def getbalance(self):
-        """Get Account Asset Balance - 資産残高を取得
-        """
+        """Get Account Asset Balance - 資産残高を取得"""
         return self.private_get("/v1/me/getbalance")
 
     def getcollateral(self):
-        """Get Margin Status - 証拠金の状態を取得
-        """
+        """Get Margin Status - 証拠金の状態を取得"""
         return self.private_get("/v1/me/getcollateral")
 
     def getcollateralaccounts(self):
-        """Obtain the details of your margin deposits for each currency - 通貨別の証拠金の数量を取得
-        """
+        """Obtain the details of your margin deposits for each currency - 通貨別の証拠金の数量を取得"""
         return self.private_get("/v1/me/getcollateralaccounts")
 
     def getaddresses(self):
-        """Get Crypto Assets Deposit Addresses - 預入用アドレス取得
-        """
+        """Get Crypto Assets Deposit Addresses - 預入用アドレス取得"""
         return self.private_get("/v1/me/getaddresses")
 
     def getcoinins(self, **kwargs):
-        """Get Crypto Assets Deposit History - 仮想通貨預入履歴
-        """
+        """Get Crypto Assets Deposit History - 仮想通貨預入履歴"""
         return self.private_get("/v1/me/getcoinins", **kwargs)
 
     def getcoinouts(self, **kwargs):
-        """Get Crypto Assets Transaction History - 仮想通貨送付履歴
-        """
+        """Get Crypto Assets Transaction History - 仮想通貨送付履歴"""
         return self.private_get("/v1/me/getcoinouts", **kwargs)
 
     def getbankaccounts(self):
-        """Get Summary of Bank Accounts - 銀行口座一覧取得
-        """
+        """Get Summary of Bank Accounts - 銀行口座一覧取得"""
         return self.private_get("/v1/me/getbankaccounts")
 
     def getdeposits(self, **kwargs):
-        """Get Cash Deposits - 入金履歴
-        """
+        """Get Cash Deposits - 入金履歴"""
         return self.private_get("/v1/me/getdeposits", **kwargs)
 
     def withdraw(self, **kwargs):
-        """Withdrawing Funds - 出金
-        """
+        """Withdrawing Funds - 出金"""
         return self.post("/v1/me/withdraw", **kwargs)
 
     def getwithdrawals(self):
-        """Get Deposit Cancellation History - 出金履歴
-        """
+        """Get Deposit Cancellation History - 出金履歴"""
         return self.private_get("/v1/me/getwithdrawals")
 
     def sendchildorder(self, **kwargs):
-        """Send a New Order - 新規注文を出す
-        """
+        """Send a New Order - 新規注文を出す"""
         return self.post("/v1/me/sendchildorder", **kwargs)
 
     def cancelchildorder(self, **kwargs):
-        """Cancel Order - 注文をキャンセルする
-        """
+        """Cancel Order - 注文をキャンセルする"""
         return self.post("/v1/me/cancelchildorder", **kwargs)
 
     def sendparentorder(self, **kwargs):
-        """Submit New Parent Order (Special order) - 新規の親注文を出す（特殊注文）
-        """
+        """Submit New Parent Order (Special order) - 新規の親注文を出す（特殊注文）"""
         return self.post("/v1/me/sendparentorder", **kwargs)
 
     def cancelparentorder(self, **kwargs):
-        """Cancel parent order - 親注文をキャンセルする
-        """
+        """Cancel parent order - 親注文をキャンセルする"""
         return self.post("/v1/me/cancelparentorder", **kwargs)
 
     def cancelallchildorders(self, **kwargs):
-        """Cancel All Orders - すべての注文をキャンセルする
-        """
+        """Cancel All Orders - すべての注文をキャンセルする"""
         return self.post("/v1/me/cancelallchildorders", **kwargs)
 
     def getchildorders(self, **kwargs):
-        """List Orders - 注文の一覧を取得
-        """
+        """List Orders - 注文の一覧を取得"""
         return self.private_get("/v1/me/getchildorders", **kwargs)
 
     def getparentorders(self, **kwargs):
-        """List Parent Orders - 親注文の一覧を取得
-        """
+        """List Parent Orders - 親注文の一覧を取得"""
         return self.private_get("/v1/me/getparentorders", **kwargs)
 
     def getparentorder(self, **kwargs):
-        """Get Parent Order Details - 親注文の詳細を取得
-        """
+        """Get Parent Order Details - 親注文の詳細を取得"""
         return self.private_get("/v1/me/getparentorder", **kwargs)
 
     def getexecutions(self, **kwargs):
-        """List Executions - 約定の一覧を取得
-        """
+        """List Executions - 約定の一覧を取得"""
         return self.private_get("/v1/me/getexecutions", **kwargs)
 
     def getbalancehistory(self, **kwargs):
-        """List Balance History - 残高履歴を取得
-        """
+        """List Balance History - 残高履歴を取得"""
         return self.private_get("/v1/me/getbalancehistory", **kwargs)
 
     def getpositions(self, **kwargs):
-        """Get Open Interest Summary - 建玉の一覧を取得
-        """
+        """Get Open Interest Summary - 建玉の一覧を取得"""
         return self.private_get("/v1/me/getpositions", **kwargs)
 
     def getcollateralhistory(self, **kwargs):
-        """Get Margin Change History - 証拠金の変動履歴を取得
-        """
+        """Get Margin Change History - 証拠金の変動履歴を取得"""
         return self.private_get("/v1/me/getcollateralhistory", **kwargs)
 
     def gettradingcommission(self, params={"product_code": "BTC_JPY"}):
-        """Get Trading Commission - 取引手数料を取得
-        """
+        """Get Trading Commission - 取引手数料を取得"""
         return self.private_get("/v1/me/gettradingcommission", params)
 
     def public_get(self, path, params=None):
         response = requests.get(self.url + path, params=params)
 
         return response
-    
+
     def private_get(self, path, params=None):
         url = self.url + path
         if params:
@@ -206,7 +181,7 @@ class API:
                 response = s.get(url, params=params)
         except requests.RequestException as e:
             raise e
-        
+
         return response
 
     def post(self, path, **kwargs):
@@ -222,7 +197,7 @@ class API:
             raise e
 
         return response
-        
+
     def get_header(self, method, path, data=None):
         timestamp = get_nonce()
         text = timestamp + method + path
@@ -236,7 +211,7 @@ class API:
             "ACCESS-SIGN": sign,
             "Content-Type": "application/json",
         }
-    
+
     def get_sign(self, text):
         return hmac.new(
             self.api_secret.encode("utf-8"), text.encode("utf-8"), hashlib.sha256
